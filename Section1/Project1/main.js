a4cdc64de09a9fa497ded5ded79f2e2050ae4cc2
BalasundaramVi <vigneshbalasundaram@gmail.com>
2019-01-14 14:37:29 -0800

    Listen to DOM and change <p> on input

--- Section1/Project1/main.js
+++ Section1/Project1/main.js
@@ -3,5 +3,12 @@ new Vue({
   el: '#app',
   data: {
     title: 'Hello World!'
+  },
+  methods: {
+    changeTitle: function(e) {
+      // this refers to the data object
+      // proxies all data properties to top vue instance
+      this.title = e.target.value;
+    }
   }
 })
