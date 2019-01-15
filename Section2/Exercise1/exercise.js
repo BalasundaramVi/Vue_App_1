0b428c9056634f745da27835f262ea9c4ee20b6e
BalasundaramVi <vigneshbalasundaram@gmail.com>
2019-01-14 19:16:43 -0800

    Update syntax for increased functionality

--- Section2/Exercise1/exercise.js
+++ Section2/Exercise1/exercise.js
@@ -2,7 +2,7 @@ new Vue({
   el: '#exercise',
   data: {
     name: 'Elondo Moosk',
-    age: (45 * 3),
+    age: 45,
     Sully: 'https://vignette.wikia.nocookie.net/pixar/images/8/8c/Sulleymonsters%2Cinc..png/revision/latest/scale-to-width-down/404?cb=20170807224356'
   },
   methods: {
