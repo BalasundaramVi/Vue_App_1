e60eaf780ccdab5eb8e89a5a67096ed90321e454
BalasundaramVi <vigneshbalasundaram@gmail.com>
2019-01-20 12:29:38 -0800

    Update computed functions to methods for click

--- Project1 - The Monster Slayer/main.js
+++ Project1 - The Monster Slayer/main.js
@@ -8,8 +8,9 @@ new Vue({
       health: 100,
     },
     gameOver: true,
-    gameLog: [{ description: 'Hello World', type: 'player-move' }, { description: 'Hello World', type: 'monster-move' }],
+    gameLog: [],
   },
+
   computed: {
     playerBarWidth: function() {
       return {
@@ -17,27 +18,52 @@ new Vue({
       }
     },
 
-    // monsterBarWidth: function() {
-    //   return {
-    //     width: (this.monster.health.toString() + '%'),
-    //   }
-    // },
+    monsterBarWidth: function() {
+      return {
+        width: (this.monster.health.toString() + '%'),
+      }
+    },
 
-    // normalAttack: function() {
-    //   let power = Math.floor(Math.random() * 20);
-    //   this.monster.health -= power;
-    //   this.monsterAttack();
-    // },
+  },
 
-    // specialAttack: function() {
-    //   let power = Math.floor(Math.random() * 50);
-    //   this.monster.health -= power;
-    //   this.monsterAttack();
-    // },
+  methods: {
+    normalAttack: function() {
+      let power = Math.floor(Math.random() * 20);
+      this.monster.health -= power;
+      this.gameLog.push({
+        description: `Player attacks monster for ${power} damage!`,
+        type: 'player-move',
+      })
+      this.monsterAttack();
+    },
 
-    // monsterAttack: function()  {
-    //   let power = Math.floor(Math.random() * 20);
-    //   this.player.health -= power;
-    // },
+    specialAttack: function() {
+      let power = Math.floor(Math.random() * 50);
+      this.monster.health -= power;
+      this.gameLog.push({
+        description: `Player uses special attack on monsterfor ${power} damage!`,
+        type: 'player-move',
+      })
+      this.monsterAttack();
+    },
+
+    monsterAttack: function()  {
+      let power = Math.floor(Math.random() * 20);
+      this.player.health -= power;
+        this.gameLog.push({
+          description: `Monster attacks player for ${power} damage!`,
+          type: 'monster-move',
+        })
+    },
+
+    heal: function() {
+      let power = Math.floor(Math.random() * 30);
+      this.player.health += power;
+      this.gameLog.push({
+        description: `Player heals for ${power} points!`,
+        type: 'player-move',
+      });
+      this.monsterAttack();
+    }
   }
 });
