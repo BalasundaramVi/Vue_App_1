cf0a8bff6712dcf4d28a7d490502ee69868776ba
BalasundaramVi <vigneshbalasundaram@gmail.com>
2019-02-01 14:54:59 -0800

    Initialize VueJS project with boilerplate setup

---
+++ Section7/vue-cli/server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const path = require('path');
+const morgan = require('morgan');
+const parser = require('body-parser');
+const favicon = require('serve-favicon');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/public')));
+
+app.use(parser.json());
+app.use(morgan('dev'));
+
+module.exports = app;
--- Section7/vue-cli/webpack.config.js
+++ Section7/vue-cli/webpack.config.js
@@ -1,12 +1,13 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './client/src/index.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
+    path: path.resolve(__dirname, './client/public/dist'),
+    publicPath: '/client/public/dist/',
+    filename: 'vue_component_bundle.js',
   },
   module: {
     rules: [
@@ -14,16 +15,15 @@ module.exports = {
         test: /\.css$/,
         use: [
           'vue-style-loader',
-          'css-loader'
+          'css-loader',
         ],
-      },      {
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          loaders: {
-          }
-          // other vue-loader options go here
-        }
+          loaders: {}
+        },
       },
       {
         test: /\.js$/,
@@ -35,40 +35,29 @@ module.exports = {
         loader: 'file-loader',
         options: {
           name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
   resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    },
     extensions: ['*', '.js', '.vue', '.json']
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: true,
-    overlay: true
+    overlay: true,
   },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map'
+  devtool: '#eval-source-map',
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.devtool = '#source-map';
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
       }
     }),
     new webpack.LoaderOptionsPlugin({
