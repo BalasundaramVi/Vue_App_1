4cf0d48c15b1e1673e9a7192f57e7f77b7889fca
BalasundaramVi <vigneshbalasundaram@gmail.com>
2019-02-02 14:16:26 -0800

    Boilerplate for Vue project

---
+++ Section8/Communication/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "plugins": [
+    "import"
+  ],
+  "rules": {
+    "no-console": 0,
+  },
+  "env": {
+    "browser": true,
+  },
+};
---
+++ Section8/Communication/server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const path = require('path');
+const morgan = require('morgan');
+const parser = require('body-parser');
+const faviocon = require('serve-favicon');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/public')));
+app.use(favicon(path.resolve(__dirname, '../client/assets/logo.png')));
+
+app.use(parser.json());
+app.use(morgan('dev'));
+
+module.exports = app;
